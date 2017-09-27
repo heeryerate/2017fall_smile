8512e867f3127ffa77dc0c3175eab61e3187497c
Judyyy <31830779+Judyyy@users.noreply.github.com>
2017-09-27 16:08:27 -0400

    add face detection code

--- src/cv_image.py
+++ src/cv_image.py
@@ -3,7 +3,28 @@ import sys
 
 image_path = "./images/test.jpg"
 
-img = cv2.imread(image_path)
+imagePath = sys.argv[1]
+cascPath = sys.argv[2]
+faceCascade = cv2.CascadeClassifier(cascPath)
+image = cv2.imread(imagePath)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+faces = faceCascade.detectMultiScale(
+    gray,
+    scaleFactor=1.1,
+    minNeighbors=5,
+    minSize=(30, 30),
+    # flags = cv2.cv.CV_HAAR_SCALE_IMAGE
+)
+print ("Found {0} faces!".format(len(faces)))
 
-cv2.imshow("face:", img)
+# Draw a rectangle around the faces
+for (x, y, w, h) in faces:
+    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+cv2.imshow("Faces found" ,image)
 cv2.waitKey(0)
+
+# img = cv2.imread(image_path)
+
+# cv2.imshow("face:", img)
+# cv2.waitKey(0)
+
