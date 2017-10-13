577d633b3cb8dc9816e5f1ddff3acd9dd65e2bde
Judyyy <31830779+Judyyy@users.noreply.github.com>
2017-10-12 23:16:42 -0400

    DataCode

---
+++ src/WebCam.py
@@ -0,0 +1,35 @@
+import cv2
+import sys
+cascPath = "./haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascPath)
+#check cam
+video_capture = cv2.VideoCapture(0)
+while True:
+    # Capture frame-by-frame
+    ret, frame = video_capture.read() #return boolean, check frame
+ 	#video_capture.isOpened()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.5,
+        minNeighbors=5,
+        #minSize=(5, 5),
+    )
+
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 105, 200), 3)
+
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
+
+
--- src/cv_image.py
+++ src/cv_image.py
@@ -13,28 +13,43 @@ import sys
 # imagePath = sys.argv[1]
 # cascPath = sys.argv[2]
 
-imagePath = "./images/dear_a.jpg"
+imagePath = "./images/test.jpg"
 cascPath = "./haarcascade_frontalface_default.xml"
 
+#face recognition
 faceCascade = cv2.CascadeClassifier(cascPath)
+#eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+
+#load the image
 image = cv2.imread(imagePath)
+
+#GreyScale
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+#detect face
+#faces = faceCascade.detectMultiScale(gray, 1.2, 5)
 faces = faceCascade.detectMultiScale(
-    gray,
-    scaleFactor=1.5,
-    minNeighbors=5,
-    minSize=(30, 30),
-    # flags = cv2.cv.CV_HAAR_SCALE_IMAGE
-)
-print ("Found {0} faces!".format(len(faces)))
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=6,
+        minSize=(6, 6),
+        
+    )
+
+
+   
+print ("Yay~ Found {0} faces!".format(len(faces)))
 
 # Draw a rectangle around the faces
 for (x, y, w, h) in faces:
-    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    cv2.rectangle(image, (x, y), (x+w, y+h), (255, 105, 200), 2)
 
 cv2.imshow("Faces found" ,image)
 
-cv2.imwrite('./images/test_faces.png',image)
+#cv2.imwrite('./images/test_faces.png',image)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+
