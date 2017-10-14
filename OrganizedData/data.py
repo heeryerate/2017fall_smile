ac964e3f63b004c5cb6da209436eee8056a20435
Xi He <xih314@lehigh.edu>
2017-10-14 14:08:04 -0400

    merge

--- OrganizedData/data.py
+++ OrganizedData/data.py
@@ -2,22 +2,23 @@ import glob #look for file
 from shutil import copyfile # copy file from one to another one
 
 emotions = ["neutral", "anger", "contempt", "disgust", "fear", "happy", "sadness", "surprise"] #Define emotion order
-participants = glob.glob("source_emotion\\*") #Returns a list of all folders with participant numbers
+participants = glob.glob("./source_emotions/*") #Returns a list of all folders with participant numbers
 
 for x in participants:
     part = "%s" %x[-4:] #store current participant number eg: S005
-    for sessions in glob.glob("%s\\*" %x): #Store list of sessions for current participant
-        for files in glob.glob("%s\\*" %sessions):
+    for sessions in glob.glob("%s/*" %x): #Store list of sessions for current participant
+        for files in glob.glob("%s/*" %sessions):
             current_session = files[20:-30]
-            file = open(files, 'r')
-            
-            emotion = int(float(file.readline())) #emotions are encoded as a float, readline as float, then convert to integer.
-            
-            sourcefile_emotion = glob.glob("source_images\\%s\\%s\\*" %(part, current_session))[-1] #get path for last image in sequence, which contains the emotion
-            sourcefile_neutral = glob.glob("source_images\\%s\\%s\\*" %(part, current_session))[0] #do same for neutral image
-            
-            dest_neut = "sorted_set\\neutral\\%s" %sourcefile_neutral[25:] #Generate path to put neutral image
-            dest_emot = "sorted_set\\%s\\%s" %(emotions[emotion], sourcefile_emotion[25:]) #Do same for emotion containing image
-            
-            copyfile(sourcefile_neutral, dest_neut) #Copy file
-            copyfile(sourcefile_emotion, dest_emot) #Copy files
+            print(current_session)
+    #         file = open(files, 'r')
+
+    #         emotion = int(float(file.readline())) #emotions are encoded as a float, readline as float, then convert to integer.
+
+    #         sourcefile_emotion = glob.glob("source_images\\%s\\%s\\*" %(part, current_session))[-1] #get path for last image in sequence, which contains the emotion
+    #         sourcefile_neutral = glob.glob("source_images\\%s\\%s\\*" %(part, current_session))[0] #do same for neutral image
+
+    #         dest_neut = "sorted_set\\neutral\\%s" %sourcefile_neutral[25:] #Generate path to put neutral image
+    #         dest_emot = "sorted_set\\%s\\%s" %(emotions[emotion], sourcefile_emotion[25:]) #Do same for emotion containing image
+
+    #         copyfile(sourcefile_neutral, dest_neut) #Copy file
+    #         copyfile(sourcefile_emotion, dest_emot) #Copy files
