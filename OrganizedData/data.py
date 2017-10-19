8d65cd2e6c2772591f97d79a1aa80c621ab9d390
Judyyy <31830779+Judyyy@users.noreply.github.com>
2017-10-18 21:49:30 -0400

    “

--- OrganizedData/data.py
+++ OrganizedData/data.py
@@ -1,24 +1,25 @@
-import glob #look for file
+# import glob #look for file
+
+import glob
 from shutil import copyfile # copy file from one to another one
-
-emotions = ["neutral", "anger", "contempt", "disgust", "fear", "happy", "sadness", "surprise"] #Define emotion order
-participants = glob.glob("./source_emotions/*") #Returns a list of all folders with participant numbers
-
-for x in participants:
-    part = "%s" %x[-4:] #store current participant number eg: S005
-    for sessions in glob.glob("%s/*" %x): #Store list of sessions for current participant
-        for files in glob.glob("%s/*" %sessions):
-            current_session = files[20:-30]
-            print(current_session)
-    #         file = open(files, 'r')
-
-    #         emotion = int(float(file.readline())) #emotions are encoded as a float, readline as float, then convert to integer.
-
-    #         sourcefile_emotion = glob.glob("source_images\\%s\\%s\\*" %(part, current_session))[-1] #get path for last image in sequence, which contains the emotion
-    #         sourcefile_neutral = glob.glob("source_images\\%s\\%s\\*" %(part, current_session))[0] #do same for neutral image
-
-    #         dest_neut = "sorted_set\\neutral\\%s" %sourcefile_neutral[25:] #Generate path to put neutral image
-    #         dest_emot = "sorted_set\\%s\\%s" %(emotions[emotion], sourcefile_emotion[25:]) #Do same for emotion containing image
-
-    #         copyfile(sourcefile_neutral, dest_neut) #Copy file
-    #         copyfile(sourcefile_emotion, dest_emot) #Copy files
+from shutil import move
+emotions = ["neutral", "anger", "contempt", "disgust", "fear", "happy", "sadness", "surprise"]
+participants = glob.glob("./source_emotion/*") 
+for x in participants: #x source_emotion/S005
+    part = x[-4:] 
+    for sessions in glob.glob("%s/*" %x): #sessions source_emotion/S005/001
+        for files in glob.glob("%s/*" %sessions): #source_emotion/S005/001/....txt
+            current_session = files[17:-30] #S005/001
+            file = open(files,'r')          
+            emotion = int(float(file.readline()))
+            pictures = glob.glob("./source_images/%s/*" %(current_session))
+            if not pictures:
+                continue
+            pictures.sort()
+                    
+            neutral_face = pictures[0]
+            emotional_face = pictures[-1]
+           
+        
+            copyfile(neutral_face, "./sorted_set/neutral/%s"%(part))
+            copyfile(emotional_face, "./sorted_set/%s/%s"%(emotions[emotion], part))
