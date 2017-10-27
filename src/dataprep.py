1de63ff962e4e1c7df70c2b6fba4e87adc2ba0d0
zhenyuli1216 <31937095+zhenyuli1216@users.noreply.github.com>
2017-10-27 00:23:10 -0400

    Zhenyu changes

---
+++ src/dataprep.py
@@ -0,0 +1,57 @@
+
+
+import numpy as np
+import os
+import cv2
+import random
+import pickle
+
+
+folderPredix = '../images/'
+cls  =['anger','disgust','fear','happy','neutral','sadness','surprise']
+
+
+y = []
+X = []
+i = 0
+for cl  in cls:
+    foldLoc = folderPredix+cl+'/'
+    for im in  os.listdir(foldLoc):
+        f = foldLoc+im
+        if im[0] != 'S':
+            continue
+        img = cv2.imread(f,-1)
+        res = cv2.resize(img,None,fx=0.1, fy=0.1, interpolation = cv2.INTER_CUBIC)
+        imM = np.array(res)
+        X.append(imM)
+        y.append(i)
+
+    i=i+1
+
+n = len(y)
+idx = [i for i in range(n)]
+
+np.random.shuffle(idx)
+
+XTr=[]
+XTe=[]
+yTr=[]
+yTe=[]
+
+for id in idx:
+    xx = np.reshape(X[id],[1,49,64])
+    
+    if random.random()<0.7:
+        XTr.append(xx)
+        yTr.append(y[id])
+    else:
+        XTe.append(xx)
+        yTe.append(y[id])
+
+with open('data.pickle', 'wb') as f:
+    data={}
+    data['XTr']=np.array(XTr)
+    data['yTr']=np.array(yTr)
+    data['XTe']=np.array(XTe)
+    data['yTe']=np.array(yTe)
+    pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)
